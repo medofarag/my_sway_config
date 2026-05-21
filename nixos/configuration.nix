7280d5ce04adf9454a198ed5dda05f9d34835e76
Mahmoud Farag <medofarag@proton.me>
2026-05-21 20:33:17 +0300

    version

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -49,13 +49,13 @@
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "ar_EG.UTF-8";
-    LC_IDENTIFICATION = "ar_EG.UTF-8";
-    LC_MEASUREMENT = "ar_EG.UTF-8";
-    LC_MONETARY = "ar_EG.UTF-8";
-    LC_NAME = "ar_EG.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "ar_EG.UTF-8";
-    LC_TELEPHONE = "ar_EG.UTF-8";
+#    LC_IDENTIFICATION = "ar_EG.UTF-8";
+#    LC_MEASUREMENT = "ar_EG.UTF-8";
+#    LC_MONETARY = "ar_EG.UTF-8";
+#    LC_NAME = "ar_EG.UTF-8";
+#    LC_NUMERIC = "en_US.UTF-8";
+#    LC_PAPER = "ar_EG.UTF-8";
+#    LC_TELEPHONE = "ar_EG.UTF-8";
     LC_TIME = "ar_EG.UTF-8";
   };
 
